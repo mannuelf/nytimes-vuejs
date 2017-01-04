72f6b586e33fd848712caaa5936674ccc6c14f98
manidf <mannuel.ferreira@gmail.com>
2017-01-04 22:36:51 +0200

    axios api call working and logging

--- src/App.vue
+++ src/App.vue
@@ -24,7 +24,7 @@ export default {
 body {
   background-color: #eee;
 }
-#app {
+#nyt-app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
--- src/components/SearchResults.vue
+++ src/components/SearchResults.vue
@@ -1,18 +1,33 @@
+
 <template>
   <div class="nyt-search-results">
-
     {{ msg }}
-
   </div>
 </template>
 
 <script>
+import Axios from 'axios';
+
 export default {
   name: 'nyt-search-results',
   data() {
     return {
       msg: 'Searching...',
     };
+  },
+  mounted() {
+    const searchQuery = 'Soccer';
+    const apiKey = '2e69849a8c1f4e76aaad0835e3e179cd'; // Be Kind Rewind: https://youtu.be/J7C8nHAAs70?t=17s > get your own API please.
+    const nytApiUrl = `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchQuery}&sort=newest&api-key=${apiKey}`;
+    const apiCall = Axios.create({
+      baseURL: nytApiUrl,
+      headers: {
+        'content-type': 'application/json',
+      },
+    });
+    // make an ajax call
+    apiCall.get(nytApiUrl)
+      .then(response => console.log(response));
   },
 };
 </script>
